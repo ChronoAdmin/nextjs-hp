762842092d2e48a2fa9446fbd4708ec9faaaaaa6
ChronoAdmin <116716565+ChronoAdmin@users.noreply.github.com>
2023-04-05 12:10:42 +0900

    Update client.js

--- libs/client.js
+++ libs/client.js
@@ -2,6 +2,6 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
   serviceDomain: "blog-nextjs-chrono",
-  apiKey: "F0uxuvvP2kjmeZrUvvnWl0V0Nhif1EaU0Kop",
+  apiKey: process.env.API_KEY,
 });
 
