bd156e6d108a105ea4e26720204039d5a58981ca
reiji_mac <r_macbook@icloud.com>
2023-04-26 21:39:02 +0900

    re

--- src/pages/hp.js
+++ src/pages/hp.js
@@ -6,25 +6,26 @@ import Image from "next/image";
 import { Loading } from "../../components/Loading";
 import Link from "next/link";
 import hoverEffect from "hover-effect";
+import { CustomSlider } from "../../components/CustomSlider";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Hp() {
+  const sliderImages = [
+    "images/hp_imgs/img1.webp",
+    "images/hp_imgs/img1_re.webp",
+    "images/hp_imgs/img2.webp",
+    "images/hp_imgs/img3_re.webp",
+    "images/hp_imgs/img3.webp",
+    "images/hp_imgs/img2_re.webp",
+  ];
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 3000);
+    const timer = setTimeout(() => setLoading(false), 1500);
     return () => clearTimeout(timer);
   }, []);
-  const imagePairs = [
-    { image1: "images/hp_imgs/img1.webp", image2: "images/blog/blogPop_1.jpg" },
-    { image1: "images/hp_imgs/img2.webp", image2: "images/blog/blogPop_2.jpg" },
-    { image1: "images/hp_imgs/img3.webp", image2: "images/blog/blogPop_3.jpg" },
-    { image1: "images/hp_imgs/img4.webp", image2: "images/blog/blogPop_4.jpg" },
-  ];
 
   const titleRef = useRef(null);
-  const titleBlogRef = useRef(null);
-  const blogLeftRef = useRef(null);
   const linksRef = useRef(null);
   const mvRef = useRef(null);
   const mv2Ref = useRef(null);
@@ -76,11 +77,6 @@ export default function Hp() {
         opacity: 1,
         duration: 2,
         ease: "power4.out",
-        scrollTrigger: {
-          trigger: "#service",
-          start: "top center",
-          end: "bottom center",
-        },
       });
     }
   }, [loading]);
@@ -91,7 +87,7 @@ export default function Hp() {
 
       <div className={styles.bgImg}>
         <Image
-          src="/images/blog/blogBg.jpg"
+          src="/images/hp_imgs/img7.webp"
           height={1080}
           width={1920}
           alt="blogページの背景画像"
@@ -155,29 +151,95 @@ export default function Hp() {
               id="card"
               ref={mv2Ref}
               style={{ height: "50vmax" }}
-            ></div>
+            >
+              <div className={styles.imgText}>
+                <h2>
+                  New HP <br />
+                  With NextJs
+                </h2>
+                <div className={styles.links}>
+                  <Link href="/">＜</Link>
+                  <Link href="/">＞</Link>
+                </div>
+              </div>
+            </div>
             <div
               className={styles.textCard1}
               id="card"
               style={{ background: "#12161e" }}
             >
-              <div className={styles.textCard1__inner}>Text Content</div>
+              <div className={styles.textCard1__inner}>
+                <div className={styles.link}>
+                  <Link href="/">hp制作のお問い合わせ</Link>
+                </div>
+                <h2>Create Home Page With Next.Js</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Recusandae, quod!
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Recusandae, quod!
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Recusandae, quod!
+                </p>
+              </div>
             </div>
             <div
               className={styles.textCard1}
               id="card"
               style={{ background: "rgb(255, 213, 0)" }}
             >
-              <div className={styles.textCard1__inner}>Text Content</div>
+              <div className={styles.textCard1__inner}>
+                <div className={styles.link}>
+                  <Link href="/">hp制作のお問い合わせ</Link>
+                </div>
+                <h2>Create Home Page With Next.Js</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Recusandae, quod!
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Recusandae, quod!
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Recusandae, quod!
+                </p>
+              </div>
             </div>
             <div
               className={styles.imgCard1}
               id="card"
               ref={mv3Ref}
               style={{ height: "50vmax" }}
-            ></div>
+            >
+              <div className={styles.imgText}>
+                <h2>
+                  New HP <br />
+                  With NextJs
+                </h2>
+                <div className={styles.links}>
+                  <Link href="/">＜</Link>
+                  <Link href="/">＞</Link>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
+        <CustomSlider
+          images={[
+            "images/hp_imgs/img1.webp",
+            "images/hp_imgs/img2.webp",
+            "images/hp_imgs/img3.webp",
+          ]}
+          interval={3000}
+          texts={["HP", "SEO", "BLOG"]}
+
+        />
       </div>
     </div>
   );
