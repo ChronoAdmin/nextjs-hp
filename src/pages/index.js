e373ae2952d992659782dd943e2da2522d242226
reiji_mac <r_macbook@icloud.com>
2023-04-29 22:27:30 +0900

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,11 @@ import { Stack } from "../../components/home/Stack";
 import { Contact } from "../../components/home/Contact";
 import { useRouter } from "next/router";
 import { Loading } from "../../components/Loading";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Mv2 } from "../../components/home/Mv2";
+import styles from "@/styles/home/index.module.css";
+import Image from "next/image";
+import hoverEffect from "hover-effect";
 // import { useEffect } from "react";
 // import { fetchDataFromApi } from "../../libs/api";
 
@@ -79,6 +82,21 @@ export default function Home({ articles, categoryUrls }) {
   // const getCategories = () => {
   //   fetchDataFromApi("api/categories?populate=*").then(res => console.log(res))
   // }
+
+  const mvRef = useRef(null);
+
+  useEffect(() => {
+    if (mvRef.current) {
+      new hoverEffect({
+        parent: mvRef.current,
+        intensity: 0.3,
+        image1: "/images/width1.webp",
+        image2: "/images/width2.webp",
+        displacementImage: "/images/map.png",
+      });
+    }
+  }, [mvRef]);
+
   return (
     <>
       {/* {loading && <Loading title="Chrono Office" />} */}
@@ -87,6 +105,43 @@ export default function Home({ articles, categoryUrls }) {
         <div className="inner">
           <About />
           <Service />
+          <div className={styles.widthArea}>
+            <div className={styles.img}>
+              <div ref={mvRef} className={styles.imgref}></div>
+              <div className={styles.imgText}>
+                <h3>
+                  <span>C</span>hrono 0ffice
+                </h3>
+                <h4>
+                  AIを事業に取り込み
+                  <br />
+                  皆様のビジネスの手助けを
+                </h4>
+                <p>
+                  マーケティングやターゲット層の特定において、AI技術を駆使しています。
+                </p>
+                <p>
+                  要件定義を適切に行い、効率的な制作プロセスに取り組むことで、プロジェクトの完成までのスピードを最適化します。{" "}
+                </p>
+              </div>
+            </div>
+            <div className={styles.text}>
+              <div className={styles.logo}>
+                <Image
+                  src="/HatchfulExport-All/logo_transparent.png"
+                  width={500}
+                  height={500}
+                  alt="logo"
+                />
+              </div>
+              <div className={styles.logo_text}>
+                <p>当社のロゴは、六角形をベースに6本の斜め線が3本ずつのグループに分かれて配置されています。</p>
+                <p>六角形は安定感やバランスを表し、斜め線はダイナミックなエネルギーと前進を象徴しています。</p>
+                <p>ホームページ制作会社として、私たちはお客様のニーズに適応し、革新的で効果的なデザインを提供することを目指しています。</p>
+                <p>このロゴは、私たちが持つ創造性と技術力を象徴し、お客様に信頼感と期待感を与えるデザインとなっています。</p>
+              </div>
+            </div>
+          </div>
           <Blog articles={articles} />
           <Stack />
           <Flow />
